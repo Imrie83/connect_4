a8aa2c01043efeba3ec4eb6c1d7ee4ac0f4bbc84
Imrie83 <czolgista83@gmail.com>
2021-10-15 15:55:32 +0200

    text based game finished

--- main.py
+++ main.py
@@ -3,39 +3,61 @@ import numpy as np
 
 ROW_COUNT = 6
 COL_COUNT = 7
+
+
 def creat_board():
     board = np.zeros((ROW_COUNT, COL_COUNT))
     return board
+
 
 board = creat_board()
 turn = 0
 game_over = False
 
+
 def drop_piece(board, row, column, piece):
     board[row][column] = piece
 
+
 def is_valid_location(board, column):
     return board[ROW_COUNT-1][column] == 0
+
 
 def get_next_open_row(board, column):
     for r in range(ROW_COUNT):
         if board[r][column] == 0:
             return r
 
+
 def print_board(board):
     print(np.flip(board, 0))
 
+
 def winning_move(board, piece):
     # horizontal location check
-    for c in range(COL_COUNT-3):
+    for c in range(COL_COUNT- 3 ):
         for r in range(ROW_COUNT):
             if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
                 return True
 
-# vertical location check
+    # vertical location check
     for c in range(COL_COUNT):
-        for r in range(ROW_COUNT-3):
+        for r in range(ROW_COUNT - 3):
             if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+
+    # positive slope check
+    for c in range(COL_COUNT - 3):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r+1][c+1] == piece \
+                and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+
+    # positive slope check
+    for c in range(COL_COUNT - 3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r - 1][c + 1] == piece \
+                and board[r - 2][c + 2] == piece and board[r - 3][c + 3] == piece:
                 return True
 
 
